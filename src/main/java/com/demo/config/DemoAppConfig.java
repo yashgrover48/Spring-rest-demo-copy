84ce301ce3c84a57f658f175b38337e470d8194f
yashgrover48 <yashgrover48@gmail.com>
2019-08-04 14:04:15 +0530

    Committing spring-rest-demo

---
+++ src/main/java/com/demo/config/DemoAppConfig.java
@@ -0,0 +1,13 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.demo.controller")
+public class DemoAppConfig implements WebMvcConfigurer {
+ 
+}
